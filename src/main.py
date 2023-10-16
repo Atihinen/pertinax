10cb92a453d6411e5b92e53eeb0d662e42bad6dd
Joonas Jauhiainen <joonas.jauhiainen@gmail.com>
2023-10-16 18:38:40 +0300

    Menu with open pdf works now

--- src/main.py
+++ src/main.py
@@ -1,18 +1,44 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, WebKit2
+gi.require_version('Poppler', '0.18')
+gi.require_version('WebKit2', '4.0')
+from gi.repository import Gtk, Poppler, WebKit2
 
 class PDFViewer(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="PDF Viewer")
         self.set_default_size(800, 600)
 
-        self.pdf_view = Gtk.ScrolledWindow()
-        self.add(self.pdf_view)
+        # Create a vertical box to hold the UI
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.add(vbox)
 
+        # Create a menu bar
+        menubar = Gtk.MenuBar()
+        vbox.pack_start(menubar, False, False, 0)
+
+        # Create a "File" menu
+        file_menu = Gtk.Menu()
+        file_menu_item = Gtk.MenuItem("File")
+        file_menu_item.set_submenu(file_menu)
+        menubar.append(file_menu_item)
+
+        # Create an "Open" option in the "File" menu
+        open_item = Gtk.MenuItem("Open")
+        open_item.connect("activate", self.on_open_item_activate)
+        file_menu.append(open_item)
+
+        # Create a scrolled window to display the PDF
+        self.pdf_view = Gtk.ScrolledWindow()
+        self.pdf_view.set_vexpand(True)
+        self.pdf_view.set_hexpand(True)
+        vbox.pack_start(self.pdf_view, True, True, 0)
+
+        # Create a WebKit2WebView to display the PDF
         self.webview = WebKit2.WebView()
         self.pdf_view.add(self.webview)
 
+    def on_open_item_activate(self, menu_item):
         # Create a FileChooserDialog to choose a PDF file
         file_dialog = Gtk.FileChooserDialog("Open PDF", self,
                                             Gtk.FileChooserAction.OPEN,
